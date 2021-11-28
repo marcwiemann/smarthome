8bd9e2581d01e7932b8b534493617b357f1c1fcb
Marc Wiemann <marc.wiemann@tk-steel.com>
2021-11-28 15:54:19 +0100

    added TestAPi to backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3003
+
+var testAPI = require('./routes/testAPI')
 
 app.use(express.urlencoded({
   extended: true
@@ -13,6 +15,7 @@ app.use(cors());
 app.get('/', (req, res)=>{
     res.send("Hier bist du leider falsch!");
 });
+app.use('/testAPI', testAPI);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
